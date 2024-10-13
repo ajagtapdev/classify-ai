7d5ff507bcaa970e110efc585fae73418223d1d2
Aarush <aarushj1300@gmail.com>
2024-10-13 10:03:14 -0700

    middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,9 +4,5 @@ export default clerkMiddleware();
 
 
 export const config = {
-  matcher: [
-    '/classifier',  // Only authenticated users can access this page
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    '/(api|trpc)(.*)', // Protect API routes if needed
-  ],
+  matcher: '/((?!_next|static|favicon.ico).*)', // Adjust matcher if needed
 };
